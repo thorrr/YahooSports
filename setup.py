f84ef2550099afa2afa5060e698e085f51065e2c
Jason Bell <jbellthor@gmail.com>
2015-12-26 23:56:50 -0500

    fix requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': '',
     'author_email': 'jbellthor@gmail.com',
     'version': '0.1',
-    'install_requires': ['pytest', 'rauth'],
+    'install_requires': ['pytest', 'rauth', 'future'],
     'packages': ['YahooSports'],
     'scripts': [],
     'name': 'YahooSports'
